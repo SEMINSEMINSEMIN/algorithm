a92b581606a35694142d353d93aa292ed20c5c8c
강세민 <ksm9802@gmail.com>
2023-06-12 09:15:29 +0900

    baekjoon - 템플릿 파일

---
+++ baekjoon/template.js
@@ -0,0 +1,13 @@
+// 답안 올리기용
+const fs = require("fs");
+const inputs = fs.readFileSync("dev/stdin").toString().trim();
+
+// 로컬
+// const fs = require("fs");
+// const inputs =
+//   process.platform === "linux"
+//     ? "dev.stdin"
+//     : fs
+//         .readFileSync(__dirname + "/example.txt")
+//         .toString()
+//         .trim();
